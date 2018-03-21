dd44f25287e79ccd8d71c9c9e0388e3d5405aacb
jfmirag <jfmirag@unal.edu.co>
2018-03-21 16:37:51 -0500

    se agregan funciones de ejemplo

--- funciones-1.R
+++ funciones-1.R
@@ -22,3 +22,88 @@ f1 <- function(x, y) {
 }
 
 f1(2,3)
+
+
+f <- function(x) x^2
+f(3)
+formals(f)
+body(f)
+environment(f)
+
+formals(f) <- alist(x=5)
+f()
+
+body(f) <- expression(x^3+100)
+f(5)
+#limpiar directorio de trabajo 
+rm(list=ls())
+
+
+f <- function() {
+  x <- 1
+  y <- 2
+  c(x, y)
+}
+
+f()
+
+rm(f)
+#################
+x <- 2
+y <- 500
+
+g <- function() {
+  y <- 1
+  c(x, y)
+}
+
+g()
+
+rm(x, g)
+################3
+
+x <- 1
+y <- 100
+h <- function() {
+  y <- 2
+  i <- function() {
+    z <- 3
+    c(x, y, z)
+  }
+  i()
+}
+h()
+rm(x, h)
+###################
+j <- function(x) {
+  y <- 2
+  function() {
+    c(x, y)
+  }
+}
+k <- j(1)
+k()
+rm(j, k)
+##########
+l <- function(x) x + 1
+
+m <- function() {
+  l <- function(x) x * 2
+  l(10)
+}
+
+m()
+
+rm(m, l)
+##############
+
+n <- function(x) x / 2
+
+o <- function() {
+  n <- 10
+  n(n)
+}
+
+o()
+
+rm(n, o)
